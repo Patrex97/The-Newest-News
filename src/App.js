39cf13f3b7a03f204217a84a10847ea13944fed3
Konrad Gmur <you@example.comegit config --global user.email konrad.gmur@gmail.com  git config --global user.name Konrad>
2020-11-08 17:22:26 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>The Newest News</h1>
+      <h2>
+        This is a new website created for people who love reading new news
+      </h2>
     </div>
   );
 }
