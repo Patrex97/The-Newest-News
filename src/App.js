b7e564410fae989628c31ca928260115ccc43353
Konrad <konrad.gmur@gmail.com>
2020-11-08 17:38:19 +0100

    change App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ function App() {
   return (
     <div className="App">
       <h1>The Newest News</h1>
-      <h2>
-        This is a new website created for people who love reading new news
-      </h2>
     </div>
   );
 }
