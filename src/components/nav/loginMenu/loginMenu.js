500f26296d1ccc6b104b036f20631f4de3d98015
Patrex97 <patrex97@gmail.com>
2020-11-22 12:29:04 +0100

    Merging changes with remote

--- src/components/nav/loginMenu/loginMenu.js
+++ src/components/nav/loginMenu/loginMenu.js
@@ -6,21 +6,29 @@ import registerImage from "./register.png";
 const LoginMenu = ({display}) => {
     useEffect(() => { 
         const loginMenu = document.querySelector(".LoginMenu");
-        display ? loginMenu.style.display = "grid" : loginMenu.style.display = "none";
+        if(display ){
+            loginMenu.classList.add("showMenu");
+            // loginMenu.classList.remove("hideMenu");
+        }else{
+            // loginMenu.classList.add("hideMenu");
+            loginMenu.classList.remove("showMenu");
+        }
     })
   return (
-    <div className="LoginMenu">
-        <div className="LoginMenu__container">
-            <button className="LoginMenu__btn">
-                <img src={loginImage} className="LoginMenu__image" alt="" />
-            </button>
-            <h3>Zaloguj się</h3>
-        </div>
-        <div className="LoginMenu__container">
-            <button className="LoginMenu__btn">
-                <img src={registerImage} className="LoginMenu__image" alt="" />
-            </button>
-            <h3>Załóż kontro</h3>
+    <div className="LoginMenu hideMenu">
+        <div className="LoginMenu__main">
+            <div className="LoginMenu__container">
+                <button className="LoginMenu__btn">
+                    <img src={loginImage} className="LoginMenu__image" alt="" />
+                </button>
+                <h3>Zaloguj się</h3>
+            </div>
+            <div className="LoginMenu__container">
+                <button className="LoginMenu__btn">
+                    <img src={registerImage} className="LoginMenu__image" alt="" />
+                </button>
+                <h3>Załóż kontro</h3>
+            </div>
         </div>
     </div>
   )
--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -10,15 +10,25 @@ const Nav = () => {
   const [loginWindow, setLoginWindow] = useState(false);
   const [hamburgerWindow, setHamburgerWindow] = useState(false);
 
+  const handleClick = e =>{
+    if(e.target.getAttribute('forComponent') === "LoginWindow"){
+      setHamburgerWindow(false);
+      setLoginWindow(!loginWindow);
+    }else{
+      setLoginWindow(false);
+      setHamburgerWindow(!hamburgerWindow);
+    }
+  }
+
   return (
     <div className="Navigation">
       <img src={logoImage} className="Navigation__logo" alt=""/>
       <input type="text" className="Navigation__searchInput"/>
-      <button className="Navigation__btn" onClick={() => setLoginWindow(!loginWindow)}>
-        <img src={userIcon} className="Navigation__icon" alt=""/>
+      <button className="Navigation__btn" forComponent="LoginWindow" onClick={handleClick}>
+        <img src={userIcon} className="Navigation__icon" alt="" forComponent="LoginWindow"/>
       </button>
-      <button className="Navigation__btn" onClick={() => setHamburgerWindow(!hamburgerWindow)}>
-        <img src={hamburgerIcon} className="Navigation__icon" alt=""/>
+      <button className="Navigation__btn" forComponent="HamburgerWindow" onClick={handleClick}>
+        <img src={hamburgerIcon} className="Navigation__icon" alt="" forComponent="HamburgerWindow"/>
       </button>
       <LoginMenu display={loginWindow} />
       <Hamburger display={hamburgerWindow}/>
